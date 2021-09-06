8925e0b2c44c96f5cd24b9272ba628e99f676631
vivek <kumarvivek313@gmail.comgit>
2021-09-06 14:16:35 +0530

    middleware

--- block-BNaabo/server/server.js
+++ block-BNaabo/server/server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
